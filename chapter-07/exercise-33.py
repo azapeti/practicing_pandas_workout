65aaeaf157a16ee310a7e21c46f876f9972d3e16
azapeti <azapeti@gmail.com>
2026-06-09 22:14:23 +0200

    exercide-33 base task done

--- chapter-07/exercise-33.py
+++ chapter-07/exercise-33.py
@@ -16,7 +16,7 @@ def _():
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ##Read in the scores file (sat-scores.csv). This time, you want the following columns: Year, State.Code, Total.Math, Family Income.Less than 20k.Math, Family Income.Between 20-40k.Math, Family Income.Between 40-60k.Math, Family Income.Between 60-80k.Math, Family Income.Between 80-100k.Math, and Family Income.More than 100k.Math.
+    ## Read in the scores file (sat-scores.csv). This time, you want the following columns: Year, State.Code, Total.Math, Family Income.Less than 20k.Math, Family Income.Between 20-40k.Math, Family Income.Between 40-60k.Math, Family Income.Between 60-80k.Math, Family Income.Between 80-100k.Math, and Family Income.More than 100k.Math.
 
     ## Rename the income-related column names to something shorter. I recommend income<20k, 20k<income<40k, 40k<income<60k, 60k<income<80k, 80k<income 100k, and income>100k.
     """)
@@ -100,9 +100,61 @@ def _(df):
         "income>100k"
     ]
 
-    percentage_change_df = df.groupby('Year')[desired_col_order].mean().transpose().pct_change().round(2)
+    percentage_change_df = df.groupby('Year')[desired_col_order].mean().transpose().pct_change().round(3)
 
     percentage_change_df
+
+    # Do you see (just by looking at the data) any income group that did worse, in any year, than the next-poorer students?
+    # My answer is NO.
+    return (percentage_change_df,)
+
+
+@app.cell
+def _(mo, percentage_change_df):
+    # Renders the classic, static Pandas HTML table with the index intact
+    mo.Html(percentage_change_df.to_html())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Which income bracket, on average, had the greatest advantage over the next-poorer income bracket?
+    """)
+    return
+
+
+@app.cell
+def _(percentage_change_df):
+    rows = list(percentage_change_df.index)
+    # rows2 = percentage_change_df.index.to_list() <- both gives the same list
+
+    rows
+    return (rows,)
+
+
+@app.cell
+def _(percentage_change_df, rows):
+    for i in rows:
+        print(f' {i:<18}| {round(percentage_change_df.loc[i].mean(), 3):>10}')
+
+    # 20k<income<40k had the greatest advantage over next-poorer income bracket
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Can we find, in a calculated and automated way, which income levels consistently (i.e., across all years) do worse than the next-poorest group?
+    """)
+    return
+
+
+@app.cell
+def _(percentage_change_df):
+    percentage_change_df[percentage_change_df < 0]
+
+    # there was never a single occasion like this
     return
 
 
